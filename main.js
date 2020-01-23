3cb13318b2ec12b41ac818b53ed137e8e9f09343
redScrapples <lauren.macdermut@gmail.com>
2020-01-23 15:51:41 -0500

    012020 IBG HW
    January 20, 2020 IBG Hw:
    Install Visual Studio Code;
    Build basic application to print name in console.

---
+++ main.js
@@ -0,0 +1,7 @@
+//Print name to console
+
+function myFirstLastName() {            //declare the function
+    console.log("Lauren MacDermut");    //instruct to print to console with specified parameter
+}
+
+myFirstLastName();      //call the function
